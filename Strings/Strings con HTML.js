3f2bb3dae58ad5515962981d139960621ec403ba
Luis14hdz <166339583+Luis14hdz@users.noreply.github.com>
2024-07-28 19:44:53 -0600

    Add files via upload

---
+++ Strings/Strings con HTML.js
@@ -0,0 +1,18 @@
+//comillas invertidos `` (backticks) me sirven para ingresar valores de una variabke dentro de una variable 
+
+let str_6 = `comillas invertidas Backticks`;
+console.log(str_6);
+
+let nombre = "Luis";
+let saludo = `Hola, ${nombre} bienvenido`
+
+console.log(saludo)
+
+//HTML
+let plantilla_html = `
+
+<html>
+    <h1> Pagina web de  ${nombre} </h1>
+</html>
+`
+console.log(plantilla_html);
